2da1a3153e1c06d53e949a851bed20aeb7e6a57f
旭霓 <xuni1024@outlook.com>
2024-11-18 12:22:58 +0800

    添加类型转换

--- agent_scheduler/task_runner.py
+++ agent_scheduler/task_runner.py
@@ -407,9 +407,10 @@ class TaskRunner:
                 time.sleep(2)
                 continue
             # 高温管控
-            temp_sleep_time = getattr(shared.opts, "high_temperature_control_time", 120)  # 获取 temp_sleep_time，默认值为 120
+            temp_sleep_time = int(getattr(shared.opts, "high_temperature_control_time", 120))
             log.info(f"\n[高温管控] 暂停{temp_sleep_time}秒等待显卡降温\n")
             time.sleep(temp_sleep_time)
+
 
             task = get_next_task()
             if not task:
